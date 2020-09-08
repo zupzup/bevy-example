72e126c88e9f5b089f458f391709898cb9496f65
Mario Zupan <mario@zupzup.org>
2020-09-08 21:07:53 +0200

    selection

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,15 @@ use bevy::{
 
 struct Unit;
 struct Name(String);
-struct Selected(bool);
-struct MouseBox;
 
-struct Position {
-    x: f32,
-    y: f32,
+#[derive(Debug)]
+struct Selected(bool);
+
+enum MouseBox {
+    Left,
+    Right,
+    Top,
+    Bottom,
 }
 
 enum Collider {
@@ -23,11 +26,19 @@ struct MouseEventsState {
     cursor_moved_event_reader: EventReader<CursorMoved>,
 }
 
+#[derive(Debug)]
+struct MouseBoxBounds {
+    top: f32,
+    bottom: f32,
+    left: f32,
+    right: f32,
+}
+
 #[derive(Default)]
 struct MouseState {
     pos: Vec2,
     pressed_at: Option<Vec2>,
-    released_at: Option<Vec2>,
+    last_bounds: Option<MouseBoxBounds>,
 }
 
 struct LogTimer(Timer);
@@ -45,12 +56,18 @@ fn main() {
 fn print_name_and_position_system(
     time: Res<Time>,
     mut timer: ResMut<LogTimer>,
-    mut query: Query<(&Unit, &Name, &Position)>,
+    mut query: Query<(&Unit, &Name, &Translation, &Selected)>,
 ) {
     timer.0.tick(time.delta_seconds);
     if timer.0.finished {
-        for (_unit, name, position) in &mut query.iter() {
-            println!("name: {}, position: {} {}", name.0, position.x, position.y);
+        for (_unit, name, translation, selected) in &mut query.iter() {
+            println!(
+                "name: {}, position: {} {}, selected: {:?}",
+                name.0,
+                translation.x(),
+                translation.y(),
+                selected
+            );
         }
     }
 }
@@ -62,6 +79,12 @@ fn setup(
 ) {
     let minka_texture = asset_server
         .load("assets/textures/minka.png")
+        .expect("can load minka");
+    let nacho_texture = asset_server
+        .load("assets/textures/nacho.png")
+        .expect("can load minka");
+    let chip_texture = asset_server
+        .load("assets/textures/chippi.png")
         .expect("can load minka");
 
     commands
@@ -79,11 +102,32 @@ fn setup(
         })
         .with(Unit)
         .with(Name("Minka".to_string()))
-        .with(Position { x: 10.0, y: 10.0 })
         .with(Collider::Solid)
         .with(Selected(false))
-        .spawn((Unit, Name("Nacho".to_string()), Position { x: 0.0, y: 0.0 }))
-        .spawn((Unit, Name("Chip".to_string()), Position { x: 5.0, y: 5.0 }));
+        .spawn(SpriteComponents {
+            material: materials.add(nacho_texture.into()),
+            translation: Translation(Vec3::new(100.0, 100.0, 0.0)),
+            sprite: Sprite {
+                size: Vec2::new(75.0, 100.0),
+            },
+            ..Default::default()
+        })
+        .with(Unit)
+        .with(Name("Nacho".to_string()))
+        .with(Collider::Solid)
+        .with(Selected(false))
+        .spawn(SpriteComponents {
+            material: materials.add(chip_texture.into()),
+            translation: Translation(Vec3::new(200.0, 200.0, 0.0)),
+            sprite: Sprite {
+                size: Vec2::new(100.0, 100.0),
+            },
+            ..Default::default()
+        })
+        .with(Unit)
+        .with(Name("Chip".to_string()))
+        .with(Collider::Solid)
+        .with(Selected(false));
 
     commands.spawn(TextComponents {
         text: Text {
@@ -163,6 +207,7 @@ impl Plugin for BasicPlugin {
             .add_startup_system(setup.system())
             .add_system(mouse_input_system.system())
             .add_system(mouse_box_system.system())
+            .add_system(selection_system.system())
             .add_system(print_name_and_position_system.system());
     }
 }
@@ -171,93 +216,152 @@ fn mouse_box_system(
     mut cmd: Commands,
     mut materials: ResMut<Assets<ColorMaterial>>,
     windows: Res<Windows>,
-    mouse_state: Res<MouseState>,
-    mut q: Query<(Entity, &MouseBox)>,
+    mouse_state: ResMut<MouseState>,
+    mut q: Query<(&mut Translation, &mut Sprite, Entity, &MouseBox)>,
 ) {
     let window = windows.get_primary();
     let win_height = window.expect("there is a window").height;
     let win_width = window.expect("there is a window").width;
-    // println!("height: {}, width: {}", win_height, win_width);
 
-    // TODO: pass in win_height and width
-    // TODO: don't spawn/despawn, but update entities
-    // TODO: pass in material, don't always add new one
     if mouse_state.pressed_at.is_some() {
-        for (e, _mb) in &mut q.iter() {
-            cmd.despawn(e);
-        }
-
         let mouse_box_material = materials.add(Color::rgb(0.2, 0.8, 0.2).into());
         let mouse_box_thickness = 1.0;
         let current = mouse_state.pos;
         let pressed_pos = mouse_state.pressed_at.expect("is there");
 
-        let start_pt_line: f32 =
-            pressed_pos.x() + ((current.x() - pressed_pos.x()) / 2.0) - win_width as f32 / 2.0;
-        // top
-        cmd.spawn(SpriteComponents {
-            material: mouse_box_material,
-            translation: Translation(Vec3::new(
-                start_pt_line,
-                pressed_pos.y() - (win_height as f32 / 2.0),
-                0.0,
-            )),
-            sprite: Sprite {
-                size: Vec2::new(current.x() - pressed_pos.x(), mouse_box_thickness),
-            },
-            ..Default::default()
-        })
-        .with(MouseBox);
-        // bottom
-        cmd.spawn(SpriteComponents {
-            material: mouse_box_material,
-            translation: Translation(Vec3::new(
-                start_pt_line,
-                current.y() - (win_height as f32 / 2.0),
-                0.0,
-            )),
-            sprite: Sprite {
-                size: Vec2::new(current.x() - pressed_pos.x(), mouse_box_thickness),
-            },
-            ..Default::default()
-        })
-        .with(MouseBox);
+        let entity_count = q.iter().iter().count();
+        if entity_count > 0 {
+            for (mut translation, mut sprite, _e, mb) in &mut q.iter() {
+                match mb {
+                    MouseBox::Top => {
+                        translation.set_x(
+                            pressed_pos.x() + ((current.x() - pressed_pos.x()) / 2.0)
+                                - win_width as f32 / 2.0,
+                        );
+                        translation.set_y(pressed_pos.y() - (win_height as f32 / 2.0));
+                        sprite.size = Vec2::new(current.x() - pressed_pos.x(), mouse_box_thickness);
+                    }
+                    MouseBox::Bottom => {
+                        translation.set_x(
+                            pressed_pos.x() + ((current.x() - pressed_pos.x()) / 2.0)
+                                - win_width as f32 / 2.0,
+                        );
+                        translation.set_y(current.y() - (win_height as f32 / 2.0));
+                        sprite.size = Vec2::new(current.x() - pressed_pos.x(), mouse_box_thickness);
+                    }
+                    MouseBox::Right => {
+                        translation.set_x(current.x() - (win_width as f32 / 2.0));
+                        translation.set_y(
+                            pressed_pos.y() + ((current.y() - pressed_pos.y()) / 2.0)
+                                - win_height as f32 / 2.0,
+                        );
+                        sprite.size = Vec2::new(mouse_box_thickness, current.y() - pressed_pos.y());
+                    }
+                    MouseBox::Left => {
+                        translation.set_x(pressed_pos.x() - (win_width as f32 / 2.0));
+                        translation.set_y(
+                            pressed_pos.y() + ((current.y() - pressed_pos.y()) / 2.0)
+                                - win_height as f32 / 2.0,
+                        );
+                        sprite.size = Vec2::new(mouse_box_thickness, current.y() - pressed_pos.y());
+                    }
+                }
+            }
+        } else {
+            let start_pt_line: f32 =
+                pressed_pos.x() + ((current.x() - pressed_pos.x()) / 2.0) - win_width as f32 / 2.0;
+            // top
+            cmd.spawn(SpriteComponents {
+                material: mouse_box_material,
+                translation: Translation(Vec3::new(
+                    start_pt_line,
+                    pressed_pos.y() - (win_height as f32 / 2.0),
+                    0.0,
+                )),
+                sprite: Sprite {
+                    size: Vec2::new(current.x() - pressed_pos.x(), mouse_box_thickness),
+                },
+                ..Default::default()
+            })
+            .with(MouseBox::Top);
+            // bottom
+            cmd.spawn(SpriteComponents {
+                material: mouse_box_material,
+                translation: Translation(Vec3::new(
+                    start_pt_line,
+                    current.y() - (win_height as f32 / 2.0),
+                    0.0,
+                )),
+                sprite: Sprite {
+                    size: Vec2::new(current.x() - pressed_pos.x(), mouse_box_thickness),
+                },
+                ..Default::default()
+            })
+            .with(MouseBox::Bottom);
 
-        let start_pt_line: f32 =
-            pressed_pos.y() + ((current.y() - pressed_pos.y()) / 2.0) - win_height as f32 / 2.0;
-        // right
-        cmd.spawn(SpriteComponents {
-            material: mouse_box_material,
-            translation: Translation(Vec3::new(
-                current.x() - (win_width as f32 / 2.0),
-                start_pt_line,
-                0.0,
-            )),
-            sprite: Sprite {
-                size: Vec2::new(mouse_box_thickness, current.y() - pressed_pos.y()),
-            },
-            ..Default::default()
-        })
-        .with(MouseBox);
-        // left
-        cmd.spawn(SpriteComponents {
-            material: mouse_box_material,
-            translation: Translation(Vec3::new(
-                pressed_pos.x() - (win_width as f32 / 2.0),
-                start_pt_line,
-                0.0,
-            )),
-            sprite: Sprite {
-                size: Vec2::new(mouse_box_thickness, current.y() - pressed_pos.y()),
-            },
-            ..Default::default()
-        })
-        .with(MouseBox);
+            let start_pt_line: f32 =
+                pressed_pos.y() + ((current.y() - pressed_pos.y()) / 2.0) - win_height as f32 / 2.0;
+            // right
+            cmd.spawn(SpriteComponents {
+                material: mouse_box_material,
+                translation: Translation(Vec3::new(
+                    current.x() - (win_width as f32 / 2.0),
+                    start_pt_line,
+                    0.0,
+                )),
+                sprite: Sprite {
+                    size: Vec2::new(mouse_box_thickness, current.y() - pressed_pos.y()),
+                },
+                ..Default::default()
+            })
+            .with(MouseBox::Right);
+            // left
+            cmd.spawn(SpriteComponents {
+                material: mouse_box_material,
+                translation: Translation(Vec3::new(
+                    pressed_pos.x() - (win_width as f32 / 2.0),
+                    start_pt_line,
+                    0.0,
+                )),
+                sprite: Sprite {
+                    size: Vec2::new(mouse_box_thickness, current.y() - pressed_pos.y()),
+                },
+                ..Default::default()
+            })
+            .with(MouseBox::Left);
+        }
     } else {
-        for (e, _mb) in &mut q.iter() {
+        for (_t, _s, e, _mb) in &mut q.iter() {
             cmd.despawn(e);
         }
     }
+}
+
+fn selection_system(
+    windows: Res<Windows>,
+    mut mouse_state: ResMut<MouseState>,
+    mut query: Query<(&Unit, &mut Selected, &Translation)>,
+) {
+    let window = windows.get_primary();
+    let win_height = window.expect("there is a window").height as f32;
+    let win_width = window.expect("there is a window").width as f32;
+
+    match mouse_state.last_bounds {
+        Some(ref bounds) => {
+            // TODO: if the bounding box is fully inside a unit, select that unit
+            for (_unit, mut selection, translation) in &mut query.iter() {
+                let unit_x = translation.x();
+                let unit_y = translation.y();
+
+                selection.0 = unit_x < (bounds.right - win_width / 2.0)
+                    && unit_x > (bounds.left - win_width / 2.0)
+                    && unit_y < (bounds.top - win_height / 2.0)
+                    && unit_y > (bounds.bottom - win_height / 2.0);
+            }
+        }
+        None => {}
+    };
+    mouse_state.last_bounds = None;
 }
 
 fn mouse_input_system(
@@ -270,21 +374,24 @@ fn mouse_input_system(
         .mouse_button_event_reader
         .iter(&mouse_button_input_events)
     {
-        // println!(
-        //     "mouse button event {:?}, position: x: {}, y: {}",
-        //     event, state.x, state.y
-        // );
         match event.state {
             ElementState::Pressed => {
+                state.last_bounds = None;
                 state.pressed_at = Some(Vec2::new(state.pos.x(), state.pos.y()));
-                state.released_at = None;
             }
             ElementState::Released => {
-                state.released_at = Some(Vec2::new(state.pos.x(), state.pos.y()));
+                let released_at = Vec2::new(state.pos.x(), state.pos.y());
+                let pressed_at = state.pressed_at.expect("is there");
                 println!(
                     "made a box from {:?} to {:?}",
-                    state.pressed_at, state.released_at
+                    state.pressed_at, released_at
                 );
+                state.last_bounds = Some(MouseBoxBounds {
+                    top: std::primitive::f32::max(pressed_at.y(), released_at.y()),
+                    bottom: std::primitive::f32::min(pressed_at.y(), released_at.y()),
+                    left: std::primitive::f32::min(pressed_at.x(), released_at.x()),
+                    right: std::primitive::f32::max(pressed_at.x(), released_at.x()),
+                });
                 state.pressed_at = None;
             }
         }
